97f9a0c7a57026442fbf940cd5b25903322304d8
Pratham <114299285+Pratham11601@users.noreply.github.com>
2025-03-09 02:39:46 +0530

    Update Router.js

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -20,10 +20,10 @@ const Router = () => {
     <Routes>
       <Route path="/" element={<Navigate to="/dashboard" />} />
       <Route path="/admin-login" element={<AdminLogin />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/sell-car" element={<SellCar />} />
-      <Route path="/settings" element={<Settings />} />
-      <Route path="/cities" element={<Cities />} />
+      <Route path="/admin/dashboard" element={<Dashboard />} />
+      <Route path="/admin/sell-car" element={<SellCar />} />
+      <Route path="/admin/settings" element={<Settings />} />
+      <Route path="/admin/cities" element={<Cities />} />
       <Route path="/category" element={<Category />} /> 
       <Route path="/sub-packages" element={<Subpackages />} /> 
       <Route path="/subscriptions" element={<Subscriptions />} /> 
