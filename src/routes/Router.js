d4a3db4e3a10a41fdaa63b4d858155a172c0ad57
Pratham <114299285+Pratham11601@users.noreply.github.com>
2025-03-10 23:39:06 +0530

    Update Router.js

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -19,7 +19,7 @@ import HomeNavigate from "../pages/HomeNavigate";
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomeNavigate />} />
+      // <Route path="/" element={<HomeNavigate />} />
       <Route path="/admin/*" element={<AdminLogin />} />
       <Route path="/admin-dasd/*" element={<Dashboard />} />
       <Route path="/sell-car" element={<SellCar />} />
